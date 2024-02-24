34e05c7553ea6f2b283cfb7761ee3c0286db5370
nneversky <skeleton.ubica@gmail.com>
2024-02-24 12:38:03 +0300

    ref main 05

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,44 +1,44 @@
-import runEngine from '../index.js';
-import getRandomInRange from '../utils.js';
+import runEngine from '../index.js'
+import getRandomInRange from '../utils.js'
 
 const randomSymbolMath = () => {
-  const symbolMath = ['-', '+', '*'];
-  return ['-', '+', '*'].at(getRandomInRange(0, symbolMath.length - 1));
-};
+    const symbolMath = ['-', '+', '*']
+    return ['-', '+', '*'].at(getRandomInRange(0, symbolMath.length - 1))
+}
 
 const calculation = (num1, num2, operator) => {
-  switch (operator) {
-    case '+':
-      return num1 + num2;
+    switch (operator) {
+        case '+':
+            return num1 + num2
 
-    case '-':
-      return num1 - num2;
+        case '-':
+            return num1 - num2
 
-    case '*':
-      return num1 * num2;
+        case '*':
+            return num1 * num2
 
-    default:
-      throw new Error(`Invalid operator - ${operator}`);
-  }
-};
+        default:
+            throw new Error(`Invalid operator - ${operator}`)
+    }
+}
 
 const generateRound = () => {
-  const num1 = getRandomInRange();
-  const num2 = getRandomInRange();
-  const operator = randomSymbolMath();
-  const question = `${num1} ${operator} ${num2}`;
-  const answer = calculation(num1, num2, operator).toString();
-  return [question, answer];
-};
+    const num1 = getRandomInRange()
+    const num2 = getRandomInRange()
+    const operator = randomSymbolMath()
+    const question = `${num1} ${operator} ${num2}`
+    const answer = calculation(num1, num2, operator).toString()
+    return [question, answer]
+}
 
 const generateArrRound = () => {
-  const countArrRounds = [];
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < 3; i++) countArrRounds.push(generateRound());
-  return countArrRounds;
-};
+    const countArrRounds = []
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < 3; i++) countArrRounds.push(generateRound())
+    return countArrRounds
+}
 
 export default () => {
-  const rules = 'What is the result of the expression?';
-  return runEngine(rules, generateArrRound());
-};
+    const rules = 'What is the result of the expression?'
+    return runEngine(rules, generateArrRound())
+}
